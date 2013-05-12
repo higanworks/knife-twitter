6775878321d71c6870bae6282c096a4a25c90c4e
sawanoboly <sawanoboriyu@higanworks.com>
2013-05-12 19:06:09 +0900

    1st commit

---
+++ lib/knife/twitter.rb
@@ -0,0 +1,7 @@
+require "knife/twitter/version"
+
+module Knife
+  module Twitter
+    # Your code goes here...
+  end
+end
---
+++ lib/knife/twitter/version.rb
@@ -0,0 +1,5 @@
+module Knife
+  module Twitter
+    VERSION = "0.0.1"
+  end
+end
